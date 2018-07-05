d8f67055c82e418690d77054efb36b9ac0e33638
Vic Zhang <Vic.Zhang@Quest.com>
2018-07-05 12:54:14 +0800

    Fix project file

--- SpotlightMobile.iOS/Properties/AssemblyInfo.cs
+++ SpotlightMobile.iOS/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpotlightMobile.iOS")]
+[assembly: AssemblyTitle("Spotlight Cloud")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SpotlightMobile.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Quest Inc.")]
+[assembly: AssemblyProduct("Spotlight Cloud")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
