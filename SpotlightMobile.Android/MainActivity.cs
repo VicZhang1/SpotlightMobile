afd616d348adc09c321cc9224c9bdf2b4b4730eb
Vic Zhang <Vic.Zhang@Quest.com>
2018-07-05 11:34:05 +0800

    Change App icon

--- SpotlightMobile.Android/MainActivity.cs
+++ SpotlightMobile.Android/MainActivity.cs
@@ -7,9 +7,9 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace SpotlightMobile.Droid
+namespace SpotlightMobile.Android
 {
-    [Activity(Label = "SpotlightMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Spotlight Cloud", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
